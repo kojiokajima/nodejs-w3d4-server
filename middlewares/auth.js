9625b6bc9b1bd7e6e07d0aa0bb38e200af7e2eb0
Koji Okajima <koji970124@gmail.com>
2021-05-25 10:36:05 -0700

    smallfix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,7 @@ const auth = (req, res, next) => {
   try {
     const token = req.header('x-auth-token')
     if(!token){
-      return res.statu(401).json({msg: "No authentication token, authorization denied."})
+      return res.status(401).json({msg: "No authentication token, authorization denied."})
     }
 
     const verified = jwt.verify(token, process.env.JWT_SIGN)
